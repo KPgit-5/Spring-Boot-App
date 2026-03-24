bf65cc37903f5b6e3e9638e3ee879421613359c1
KPgit-5 <pandeykalpana890@gmail.com>
2026-03-24 21:13:19 +0530

    Update HelloController.java

--- src/main/java/com/devops/demo/HelloController.java
+++ src/main/java/com/devops/demo/HelloController.java
@@ -1,13 +1,40 @@
 package com.devops.demo;
 
 import org.springframework.web.bind.annotation.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Map;
 
 @RestController
 public class HelloController {
 
-@GetMapping("/")
-public String hello() {
-return "Hello from DevOps ECS Project!";
-}
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from DevOps ECS Project!";
+    }
 
+    @GetMapping("/db-test")
+    public String testDB() {
+        try {
+            Map<String, String> secret = SecretsManagerService.getSecret();
+
+            String host = secret.get("host");
+            String username = secret.get("username");
+            String password = secret.get("password");
+            String dbname = secret.get("dbname");
+            String port = secret.get("port");
+            String url = "jdbc:mysql://" + host + ":" + port + "/" + dbname;
+
+            Connection conn = DriverManager.getConnection(url, username, password);
+
+            if (conn != null) {
+                return "✅ DB Connected Successfully!";
+            } else {
+                return "❌ DB Connection Failed";
+            }
+
+        } catch (Exception e) {
+            return "❌ Error: " + e.getMessage();
+        }
+    }
 }
